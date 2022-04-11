aa6925e9e04df2fb56b29036323f375e687bdb27
nasimmdemon <nasimmdemon@gmail.com>
2022-04-11 13:40:54 +0600

    Component Create Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import MainComponent from "./Components/MainComponent";
 
 function App() {
   return (
     <div className="App">
-       
+       <MainComponent />
     </div>
   );
 }
---
+++ src/Components/MainComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header/Header.jsx'
+import Body from './body/Body.jsx'
+import Footer from './Footer/Footer.jsx'
+
+const MainComponent = () => {
+    return (
+        <div>
+            <Header/>
+            <Body />
+            <Footer />
+
+        </div>
+    );
+};
+
+export default MainComponent;
